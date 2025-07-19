5c2395828f99b0c73c89d5e49c21d297b4dc05ce
ryu-ryuk <meow@alokranjan.me>
2025-07-19 16:26:58 +0530

    update scripts

--- gen.py
+++ gen.py
@@ -1,12 +1,16 @@
 import os
 
-## to auto update the readme with newly pushed wallpapers
+# to auto update the readme with newly pushed wallpapers
 output_file = "WALLPAPER_GALLERY.md"
-images = [f for f in os.listdir('.') if f.lower().endswith(('.jpg', '.jpeg', '.png', '.webp'))]
+images = [
+    f
+    for f in os.listdir(".")
+    if f.lower().endswith((".jpg", ".jpeg", ".png", ".webp", ".gif"))
+]
 
 images.sort()
 cols = 3
-rows = [images[i:i + cols] for i in range(0, len(images), cols)]
+rows = [images[i : i + cols] for i in range(0, len(images), cols)]
 
 with open(output_file, "w") as f:
     f.write("## Wallpaper Gallery\n\n")
